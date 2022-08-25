bbb4a5a3a8fa42a5aa9516943d02ed02d55f56f6
Nazmul Hasan Himel <himelspark96@gmail.com>
2022-08-25 10:36:20 +0600

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Quiz from "./Components/Quiz";
+import { QuizProvider } from "./Contexts/QuizContext";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <QuizProvider>
+      <Quiz />
+    </QuizProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
